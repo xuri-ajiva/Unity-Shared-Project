f8c45abf7ddbcc9c08019e10f317fad17b892443
= <=>
2019-10-30 19:23:52 +0100

    init

---
+++ UnityProject/Assets/XTCode/NetPlayerControler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetPlayerControler : NetworkBehaviour
+{
+
+    public GameObject PlayerUnitPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //is this my unit ?
+        if (!hasAuthority) return;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameObject.transform.Translate(0, 1, 0);
+        }
+    }
+
+
+
+}
---
+++ UnityProject/Assets/XTCode/PlayerConnectUnitScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerConnectUnitScript : NetworkBehaviour
+{
+    public GameObject PlayerUnitPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //my inut ?
+        if (!isLocalPlayer) return;
+
+        CmdSpawnPlayerUnit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /////////////////////////// COMMANDS
+    /// This is Runing on the server 
+    /// 
+    [Command]
+    void CmdSpawnPlayerUnit()
+    {
+        var go = Instantiate(PlayerUnitPrefab);
+
+        NetworkServer.SpawnWithClientAuthority(go, connectionToClient);
+
+    }
+}
