72ccab3a8f1e876be9256f8deefefe24eb627e05
xuri <t0bl4s.durst@gmail.com>
2019-11-03 22:36:48 +0100

    try to connect terain maps

--- SiToJa/Assets/XTCode/TerainGenerator.cs
+++ SiToJa/Assets/XTCode/TerainGenerator.cs
@@ -2,9 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XTCode;
 using Random = System.Random;
 
 public class TerainGenerator : MonoBehaviour {
+    Noise _noise = new Noise();
+
     public int depth = 20;
 
     public float scala = 2F;
@@ -22,10 +25,10 @@ public class TerainGenerator : MonoBehaviour {
     public List<GameObject> GameObjectsToTrack = new List<GameObject>();
 
     public bool[,] TerainMap = new Boolean[CHUNK_SIZE, CHUNK_SIZE];
-    
+
     private       int delay             = 0;
     private const int TERRAIN_GEN_RANGE = 2;
-    
+
 
     //Terrain Get(int x, int y) { return this.TerainMap[(int) ( x + ( CHUNK_SIZE / 2 ) ), (int) ( y + ( CHUNK_SIZE / 2 ) )]; }
 
@@ -49,12 +52,12 @@ public class TerainGenerator : MonoBehaviour {
 
     private float[,] GenerateHeights(int chunkX, int chunkY) {
         float[,] heigts = new float[CHUNK_SIZE, CHUNK_SIZE];
-        var baseX = ( CHUNK_SIZE * chunkX );
-        var baseY = ( CHUNK_SIZE * chunkY );
+        var      baseX  = ( CHUNK_SIZE * chunkX );
+        var      baseY  = ( CHUNK_SIZE * chunkY );
 
         for ( int i = 0; i < CHUNK_SIZE; i++ ) {
             for ( int j = 0; j < CHUNK_SIZE; j++ ) {
-                var k = CalculateHeigth(  baseX+ i, baseY + j ); //Perlinnoide
+                var k = CalculateHeigth( baseX + i, baseY + j ); //Perlinnoide
                 heigts[i, j] = k;
                 SpawnUnit( i, k, j );
             }
@@ -66,7 +69,7 @@ public class TerainGenerator : MonoBehaviour {
     private float CalculateHeigth(int absX, int absY) {
         float xPos = (float) absX / CHUNK_SIZE * scala;
         float yPos = (float) absY / CHUNK_SIZE * scala;
-        
+
         //float xPos = (float) absX * (1 / scala);
         //float yPos = (float) absY * (1 / scala);
 
@@ -102,7 +105,7 @@ public class TerainGenerator : MonoBehaviour {
                 for ( int z = -TERRAIN_GEN_RANGE; z < TERRAIN_GEN_RANGE; z++ ) {
                     if ( !terainAvailable( xChunk + x, zChunk + z ) ) {
                         requestTerain( xChunk + x, zChunk + z );
-                        Debug.Log( $"Requested: [{xChunk + x},{zChunk + z}]"   );
+                        Debug.Log( $"Requested: [{xChunk + x},{zChunk + z}]" );
                     }
                 }
             }
@@ -124,13 +127,14 @@ public class TerainGenerator : MonoBehaviour {
         var tColider = go.GetComponent<TerrainCollider>();
 
         TerrainData tData = new TerrainData();
-        tData.heightmapResolution = CHUNK_SIZE + 1;
-        tData.size                = new Vector3( CHUNK_SIZE, CHUNK_SIZE / this.depthDevider, CHUNK_SIZE );
-        tData.SetHeights( 0, 0, GenerateHeights( chunkX * CHUNK_SIZE, chunkY * CHUNK_SIZE ) );
+        tData.heightmapResolution = CHUNK_SIZE + 3;
+        tData.size                = new Vector3( CHUNK_SIZE+1, CHUNK_SIZE / this.depthDevider, CHUNK_SIZE+1 );
+        tData.SetHeights( 0, 0, GenerateHeights( chunkX , chunkY  ) );
+        //tData.SetHeights( 0, 0, Noise.GenerateNoiseMap( CHUNK_SIZE+1, CHUNK_SIZE+1, 1, this.scala, new Vector2( CHUNK_SIZE * chunkX, CHUNK_SIZE * chunkY ) ) );
         tColider.terrainData  = tData;
         tBase.terrainData     = tData;
         go.transform.position = new Vector3( CHUNK_SIZE * chunkX, 0, CHUNK_SIZE * chunkY );
-        go.name = chunkX + ", " + chunkY;
+        go.name               = chunkX + ", " + chunkY;
         SetterainAvailable( chunkX, chunkY );
     }
 }
