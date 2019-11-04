06682b49bcaca3f1a1135d0bd0039121fd79b08e
xuri <t0bl4s.durst@gmail.com>
2019-11-04 18:07:07 +0100

    terain fix

--- SiToJa/Assets/XTCode/PlayerConectionScript.cs
+++ SiToJa/Assets/XTCode/PlayerConectionScript.cs
@@ -1,74 +1,73 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Networking;
 using static XTCode.XConst;
 
-public class PlayerConectionScript : NetworkBehaviour {
-    public GameObject PlayerCntrolerPrefab;
+namespace XTCode {
+    public class PlayerConectionScript : NetworkBehaviour {
+        public GameObject PlayerCntrolerPrefab;
 
-    //syncs Variable To all Clients
-    //hock overrides function
-    [SyncVar(hook = nameof(HookUpdateName))]
-    public string PlayerName;
+        //syncs Variable To all Clients
+        //hock overrides function
+        [SyncVar(hook = nameof(HookUpdateName))]
+        public string PlayerName;
 
-    // Start is called before the first frame update
-    void Start() {
-        if (!isLocalPlayer) return;
+        // Start is called before the first frame update
+        void Start() {
+            if (!this.isLocalPlayer) return;
 
-        CmdSpawnPlayerUnit();
-    }
-
-    // Update is called once per frame
-    void Update() {
-        if (!isLocalPlayer) return;
-
-        if (Input.GetKeyDown(KeyCode.N)) {
-            var n = Random.Range(0, int.MaxValue);
-            CmdChangePlayerName(name);
-        }
-    }
-
-    void ChangePlayerName(string Name) { }
-
-    //////////////////////////////////// COMMANDS
-    /// Only Runs On The server
-    [Command]
-    //must start with Cmd
-    void CmdSpawnPlayerUnit() {
-        Debug.Log("CmdSpawnPlayerUnit: Spawn Unit for " + connectionToClient);
-        var obj = Instantiate(PlayerCntrolerPrefab);
-
-        //Tell all clients from the new obj and the owner!
-        NetworkServer.SpawnWithClientAuthority(obj, connectionToClient);
-    }
-
-    [Command]
-    void CmdChangePlayerName(string name) {
-        Debug.Log($"CmdChangePlayerName: Request to change Players name to [{name}] for: " + connectionToClient);
-
-        //check plyers Name Length
-        if (name.Length < MINIMUM_NAME_LENGTH) {
-            Debug.LogError($"CmdChangePlayerName: Players name is to short:[{name.Length}]!");
-            return;
+            CmdSpawnPlayerUnit();
         }
 
-        //check plyers Name Length
-        if (name.Length > MAXIMUM_NAME_LENGTH) {
-            Debug.LogError($"CmdChangePlayerName: Players name is to long:[{name.Length}]!");
-            return;
+        // Update is called once per frame
+        void Update() {
+            if (!this.isLocalPlayer) return;
+
+            if (Input.GetKeyDown(KeyCode.N)) {
+                var n = Random.Range(0, int.MaxValue);
+                CmdChangePlayerName(this.name);
+            }
         }
 
-        //TODO: Filter for blacklisted Words
+        void ChangePlayerName(string Name) { }
 
-        //because SyncVar Updates On all Clients: Calls HookUpdateName
-        PlayerName = name;
-    }
+        //////////////////////////////////// COMMANDS
+        /// Only Runs On The server
+        [Command]
+        //must start with Cmd
+        void CmdSpawnPlayerUnit() {
+            Debug.Log("CmdSpawnPlayerUnit: Spawn Unit for " + this.connectionToClient);
+            var obj = Instantiate(this.PlayerCntrolerPrefab);
 
-    ////////////////////////////////// RPC
-    /// Only runs on Clients
+            //Tell all clients from the new obj and the owner!
+            NetworkServer.SpawnWithClientAuthority(obj, this.connectionToClient);
+        }
 
-    /*[ClientRpc]
+        [Command]
+        void CmdChangePlayerName(string name) {
+            Debug.Log($"CmdChangePlayerName: Request to change Players name to [{name}] for: " + this.connectionToClient);
+
+            //check plyers Name Length
+            if (name.Length < MINIMUM_NAME_LENGTH) {
+                Debug.LogError($"CmdChangePlayerName: Players name is to short:[{name.Length}]!");
+                return;
+            }
+
+            //check plyers Name Length
+            if (name.Length > MAXIMUM_NAME_LENGTH) {
+                Debug.LogError($"CmdChangePlayerName: Players name is to long:[{name.Length}]!");
+                return;
+            }
+
+            //TODO: Filter for blacklisted Words
+
+            //because SyncVar Updates On all Clients: Calls HookUpdateName
+            this.PlayerName = name;
+        }
+
+        ////////////////////////////////// RPC
+        /// Only runs on Clients
+
+        /*[ClientRpc]
     void RpcUpdateName(string name)
     {
         Debug.Log($"RpcUpdateName: Request To Chaneg Name From [{PlayerName}] to [{name}].");
@@ -77,11 +76,12 @@ public class PlayerConectionScript : NetworkBehaviour {
 */
 
 
-    //////////////////////////////// HOOKS
-    /// Custom Hocks
-    void HookUpdateName(string name) {
-        Debug.Log($"HookUpdateName: Request To change Name from [{PlayerName}] to [{name}].");
-        //because we override the SyncVar Rcp we need to set the variable self 
-        PlayerName = name;
+        //////////////////////////////// HOOKS
+        /// Custom Hocks
+        void HookUpdateName(string name) {
+            Debug.Log($"HookUpdateName: Request To change Name from [{this.PlayerName}] to [{name}].");
+            //because we override the SyncVar Rcp we need to set the variable self 
+            this.PlayerName = name;
+        }
     }
 }
--- SiToJa/Assets/XTCode/TerainGenerator.cs
+++ SiToJa/Assets/XTCode/TerainGenerator.cs
@@ -1,140 +1,149 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using XTCode;
 using Random = System.Random;
 
-public class TerainGenerator : MonoBehaviour {
-    Noise _noise = new Noise();
+namespace XTCode {
+    public class TerainGenerator : MonoBehaviour {
+        private Noise _noise = new Noise();
 
-    public int depth = 20;
+        public int depth = 20;
 
-    public float scala = 2F;
+        public float scala = 2F;
 
-    public        List<GameObject> GameObjectsToSpawn;
-    public        bool             SpawnObjets;
-    private const double           TOLERANCE_SPAWN = 1F;
+        public        List<GameObject> GameObjectsToSpawn;
+        public        bool             SpawnObjets;
+        private const double           TOLERANCE_SPAWN = 1F;
 
-    public GameObject TerainRoot;
+        public GameObject TerainRoot;
 
-    public const int        CHUNK_SIZE   = 128;
-    public       int        depthDevider = 4;
-    public       GameObject Example;
-
-    public List<GameObject> GameObjectsToTrack = new List<GameObject>();
-
-    public bool[,] TerainMap = new Boolean[CHUNK_SIZE, CHUNK_SIZE];
-
-    private       int delay             = 0;
-    private const int TERRAIN_GEN_RANGE = 2;
+        public const int        CHUNK_SIZE   = 129;
+        public       int        depthDevider = 4;
+        public       GameObject Example;
 
 
-    //Terrain Get(int x, int y) { return this.TerainMap[(int) ( x + ( CHUNK_SIZE / 2 ) ), (int) ( y + ( CHUNK_SIZE / 2 ) )]; }
+        public bool             trackObjects;
+        public List<GameObject> GameObjectsToTrack = new List<GameObject>();
 
-    //void Set(int x, int y, Terrain t) { this.TerainMap[(int) ( x + ( CHUNK_SIZE / 2 ) ), (int) ( y + ( CHUNK_SIZE / 2 ) )] = new Terrain(); }
+        public bool[,] TerainMap = new Boolean[CHUNK_SIZE, CHUNK_SIZE];
 
-    private Random rd = new Random();
+        private       int delay             = 0;
+        private const int TERRAIN_GEN_RANGE = 2;
 
 
-    // Start is called before the first frame update
-    void Start() {
-        for ( int i = 0; i < CHUNK_SIZE; i++ ) {
-            for ( int j = 0; j < CHUNK_SIZE; j++ ) {
-                this.TerainMap[i, j] = false;
+        //Terrain Get(int x, int y) { return this.TerainMap[(int) ( x + ( CHUNK_SIZE / 2 ) ), (int) ( y + ( CHUNK_SIZE / 2 ) )]; }
+
+        //void Set(int x, int y, Terrain t) { this.TerainMap[(int) ( x + ( CHUNK_SIZE / 2 ) ), (int) ( y + ( CHUNK_SIZE / 2 ) )] = new Terrain(); }
+
+        private Random rd = new Random();
+
+
+        // Start is called before the first frame update
+        private void Start() {
+            for ( int i = 0; i < CHUNK_SIZE; i++ ) {
+                for ( int j = 0; j < CHUNK_SIZE; j++ ) {
+                    this.TerainMap[i, j] = false;
+                }
+            }
+
+            GenTeraun();
+        }
+
+        private void GenTeraun() {
+            for ( int i = 0; i < 4; i++ ) {
+                for ( int j = 0; j < 4; j++ ) {
+                    requestTerrain( 2 - i, 2 - j );
+                }
             }
         }
 
-        GenTeraun();
-    }
+        private float[,] GenerateHeights(int chunkX, int chunkY) {
+            float[,] heigts = new float[CHUNK_SIZE, CHUNK_SIZE];
+//        var      baseX  = ( CHUNK_SIZE * chunkX );
+//        var      baseY  = ( CHUNK_SIZE * chunkY );
 
-    private void GenTeraun() { requestTerain( 0, 0 ); }
+            for ( int i = 0; i < CHUNK_SIZE; i++ ) {
+                for ( int j = 0; j < CHUNK_SIZE; j++ ) {
+                    var k = CalculateHeigth( i, j, chunkX, chunkY ); //Perlinnoide
+                    heigts[j, i] = k;
+                    SpawnUnit( i, k, j );
+                }
+            }
 
-    private float[,] GenerateHeights(int chunkX, int chunkY) {
-        float[,] heigts = new float[CHUNK_SIZE, CHUNK_SIZE];
-        var      baseX  = ( CHUNK_SIZE * chunkX );
-        var      baseY  = ( CHUNK_SIZE * chunkY );
+            return heigts;
+        }
 
-        for ( int i = 0; i < CHUNK_SIZE; i++ ) {
-            for ( int j = 0; j < CHUNK_SIZE; j++ ) {
-                var k = CalculateHeigth( baseX + i, baseY + j ); //Perlinnoide
-                heigts[i, j] = k;
-                SpawnUnit( i, k, j );
+        private float CalculateHeigth(int absX, int absY, int chunkX, int chunkY) {
+            float xPos = (float) absX / CHUNK_SIZE;
+            float yPos = (float) absY / CHUNK_SIZE;
+
+            //float xPos = (float) absX * (1 / scala);
+            //float yPos = (float) absY * (1 / scala);
+
+            return Mathf.PerlinNoise( ( xPos + chunkX ) * this.scala, ( yPos + chunkY ) * this.scala );
+        }
+
+        private void SpawnUnit(int x, float y, int z) {
+            if ( this.SpawnObjets ) {
+                if ( Math.Abs( ( Mathf.PerlinNoise( x / this.scala, y / this.scala ) * this.scala / 2 ) - z ) <= TOLERANCE_SPAWN ) {
+                    Debug.Log( "1" );
+
+                    var o  = this.rd.Next( 0, this.GameObjectsToSpawn.Count );
+                    var go = Instantiate( this.GameObjectsToSpawn[o], this.transform, true );
+
+                    var t = this.transform.position;
+
+                    go.transform.position = new Vector3( x + t.x, y, z + t.z );
+                }
             }
         }
 
-        return heigts;
-    }
+        private void Update() {
+            if ( !this.trackObjects ) return;
+            this.delay++;
+            if ( this.delay <= CHUNK_SIZE ) return;
 
-    private float CalculateHeigth(int absX, int absY) {
-        float xPos = (float) absX / CHUNK_SIZE * scala;
-        float yPos = (float) absY / CHUNK_SIZE * scala;
+            foreach ( var o in this.GameObjectsToTrack ) {
+                var pos = o.transform.position / CHUNK_SIZE;
 
-        //float xPos = (float) absX * (1 / scala);
-        //float yPos = (float) absY * (1 / scala);
+                int xChunk = (int) pos.x;
+                int zChunk = (int) pos.z;
 
-        return Mathf.PerlinNoise( xPos, yPos );
-    }
-
-    private void SpawnUnit(int x, float y, int z) {
-        if ( SpawnObjets ) {
-            if ( Math.Abs( ( Mathf.PerlinNoise( x / scala, y / scala ) * scala / 2 ) - z ) <= TOLERANCE_SPAWN ) {
-                Debug.Log( "1" );
-
-                var o  = rd.Next( 0, GameObjectsToSpawn.Count );
-                var go = Instantiate( GameObjectsToSpawn[o], this.transform, true );
-
-                var t = this.transform.position;
-
-                go.transform.position = new Vector3( x + t.x, y, z + t.z );
-            }
-        }
-    }
-
-    void Update() {
-        this.delay++;
-        if ( this.delay <= CHUNK_SIZE ) return;
-
-        foreach ( var o in this.GameObjectsToTrack ) {
-            var pos = o.transform.position / CHUNK_SIZE;
-
-            int xChunk = (int) pos.x;
-            int zChunk = (int) pos.z;
-
-            for ( int x = -TERRAIN_GEN_RANGE; x < TERRAIN_GEN_RANGE; x++ ) {
-                for ( int z = -TERRAIN_GEN_RANGE; z < TERRAIN_GEN_RANGE; z++ ) {
-                    if ( !terainAvailable( xChunk + x, zChunk + z ) ) {
-                        requestTerain( xChunk + x, zChunk + z );
-                        Debug.Log( $"Requested: [{xChunk + x},{zChunk + z}]" );
+                for ( int x = -TERRAIN_GEN_RANGE; x < TERRAIN_GEN_RANGE; x++ ) {
+                    for ( int z = -TERRAIN_GEN_RANGE; z < TERRAIN_GEN_RANGE; z++ ) {
+                        if ( !terrainAvailable( xChunk + x, zChunk + z ) ) {
+                            requestTerrain( xChunk + x, zChunk + z );
+                            Debug.Log( $"Requested: [{xChunk + x},{zChunk + z}]" );
+                        }
                     }
                 }
             }
         }
-    }
 
-    private bool terainAvailable(int posX, int posY) {
-        Debug.Log( posX + " - "    + posY );
-        return this.TerainMap[posX + ( CHUNK_SIZE / 2 ), posY + ( CHUNK_SIZE / 2 )];
-    }
+        private bool terrainAvailable(int posX, int posY) {
+            Debug.Log( posX + " - "    + posY );
+            return this.TerainMap[posX + ( CHUNK_SIZE / 2 ), posY + ( CHUNK_SIZE / 2 )];
+        }
 
-    private void SetterainAvailable(int posX, int posY) { this.TerainMap[posX + ( CHUNK_SIZE / 2 ), posY + ( CHUNK_SIZE / 2 )] = true; }
+        private void SetTerrainAvailable(int posX, int posY) { this.TerainMap[posX + ( CHUNK_SIZE / 2 ), posY + ( CHUNK_SIZE / 2 )] = true; }
 
-    void requestTerain(int chunkX, int chunkY) {
-        if ( terainAvailable( chunkX, chunkY ) ) return;
+        private void requestTerrain(int chunkX, int chunkY) {
+            if ( terrainAvailable( chunkX, chunkY ) ) return;
 
-        var go       = Instantiate( this.Example, this.TerainRoot.transform, true );
-        var tBase    = go.GetComponent<Terrain>();
-        var tColider = go.GetComponent<TerrainCollider>();
+            var go       = Instantiate( this.Example, this.TerainRoot.transform, true );
+            var tBase    = go.GetComponent<UnityEngine.Terrain>();
+            var tColider = go.GetComponent<TerrainCollider>();
 
-        TerrainData tData = new TerrainData();
-        tData.heightmapResolution = CHUNK_SIZE + 3;
-        tData.size                = new Vector3( CHUNK_SIZE+1, CHUNK_SIZE / this.depthDevider, CHUNK_SIZE+1 );
-        tData.SetHeights( 0, 0, GenerateHeights( chunkX , chunkY  ) );
-        //tData.SetHeights( 0, 0, Noise.GenerateNoiseMap( CHUNK_SIZE+1, CHUNK_SIZE+1, 1, this.scala, new Vector2( CHUNK_SIZE * chunkX, CHUNK_SIZE * chunkY ) ) );
-        tColider.terrainData  = tData;
-        tBase.terrainData     = tData;
-        go.transform.position = new Vector3( CHUNK_SIZE * chunkX, 0, CHUNK_SIZE * chunkY );
-        go.name               = chunkX + ", " + chunkY;
-        SetterainAvailable( chunkX, chunkY );
+            TerrainData tData = new TerrainData();
+            tData.heightmapResolution = CHUNK_SIZE;
+            tData.size                = new Vector3( CHUNK_SIZE, CHUNK_SIZE / this.depthDevider, CHUNK_SIZE );
+            tData.SetHeights( 0, 0, GenerateHeights( chunkX, chunkY ) );
+            //tData.SetHeights( 0, 0, Noise.GenerateNoiseMap( CHUNK_SIZE+1, CHUNK_SIZE+1, 1, this.scala, new Vector2( CHUNK_SIZE * chunkX, CHUNK_SIZE * chunkY ) ) );
+            tColider.terrainData  = tData;
+            tBase.terrainData     = tData;
+            go.transform.position = new Vector3( CHUNK_SIZE * chunkX, 0, CHUNK_SIZE * chunkY );
+            go.name               = chunkX + ", " + chunkY;
+            SetTerrainAvailable( chunkX, chunkY );
+        }
     }
 }
