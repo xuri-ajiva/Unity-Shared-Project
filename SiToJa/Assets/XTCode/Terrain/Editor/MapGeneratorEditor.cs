b1fcf4fb2c8a3218e634de8b3b23137d050ce89a
xuri <t0bl4s.durst@gmail.com>
2019-11-07 20:12:42 +0100

    terain generator 2.0 begin

---
+++ SiToJa/Assets/XTCode/Terrain/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MapGeneratorEditor : Editor {
+    // Start is called before the first frame update
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() { }
+}
